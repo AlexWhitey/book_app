058a96d292aabe6e5ab45c9305af52355fdd7b9f
IanGifford261 <lightyagamie10@gmail.com>
2019-03-01 10:56:54 -0800

    stuff to pull from master

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 // For the save book button
 
-$('#formButton').toggle(function() {
-  $('#hidden-form').addClass('active');
-}, function () {
-  $('#hidden-form').removeClass('active')
-});
-
-// $('#formButton').click(function(){
-//   $('#hidden-form').toggle();
+// $('#formButton').toggle(function() {
+//   $('#hidden-form').addClass('active');
+// }, function () {
+//   $('#hidden-form').removeClass('active')
 // });
+
+$('#formButton').click(function(){
+  $('.hidden-field').toggle();
+});
